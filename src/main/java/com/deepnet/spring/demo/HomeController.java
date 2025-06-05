ae02f3dfbe6fb53c09ffb20dc36d2c2669a1f4f8
Yifan Liu <yifanliu.uk@gmail.com>
2025-06-05 20:21:50 +0100

    new method

--- src/main/java/com/deepnet/spring/demo/HomeController.java
+++ src/main/java/com/deepnet/spring/demo/HomeController.java
@@ -1,6 +1,7 @@
 package com.deepnet.spring.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,11 @@ public class HomeController {
     public String sayHello() {
         return "Hello, AWS";
     }
+
+    @GetMapping(path = {"/{who}", "{who}", "/home/{who}"})
+    public String sayHelloTo(@PathVariable String who) {
+        return "Hello, " + who;
+    }
+
 
 }
